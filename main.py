636519c85efbc0fc92832812216df3d919c4062e
LEE JI HUN <dlwlgns0517@gmail.com>
2022-10-31 15:19:19 +0900

    20221031
    Created project

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 import random
 
-num = random.randint(1, 2)
-print(num)
+num = random.randint(1, 100)
+life = 6
+
+while life > 0:
+    guess = int(input("guess number: "))
+    life -= 1
+    if guess > num:
+        print(f"Lower than {guess} life {life}")
+    elif guess < num:
+        print(f"Higher than {guess} life {life}")
+    else:
+        print("Game clear")
+        break
+
+if life == 0:
+    print("Game Over")
